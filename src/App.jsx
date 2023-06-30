c5c222c2b5d18bd1d39b160b77ce046e39c53389
Sreeprada6303 <sreeprada632003@gmail.com>
2023-06-30 22:28:03 +0530

    header

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Header'
+import Slider from './Components/slider'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+        <Header/>
+
+        <Slider/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/slider.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect,useState} from 'react'
+import GlobalApi from '../Services/GlobalApi'
+const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+function Slider() {
+    const [movieList,setMovieList]=useState([]);
+    useEffect(()=>{
+        getTrendingVideos();
+    },[])
+
+    const getTrendingVideos=()=>{
+        GlobalApi.getTrendingVideos.then(resp=>{
+            console.log(resp.data.results);
+            setMovieList(resp.data.results)
+        })
+    }
+
+    return (
+        <div className='flex overflow-x-auto'>
+            {movieList.map((item,index)=>(
+                <img src={IMAGE_BASE_URL+item.backdrop_path}
+                className='min-w-full h-[310px] object-cover
+                object-left-top'/>
+            ))}
+        </div>
+    )
+}
+
+export default Slider
